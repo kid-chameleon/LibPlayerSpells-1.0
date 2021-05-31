42573de782f2ae77ebbf0c0c2d453fddef1fa9b5
kid-chameleon <56284637+kid-chameleon@users.noreply.github.com>
2021-05-31 15:23:34 -0700

    blerg

--- data/Shaman.lua
+++ data/Shaman.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0. If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub('LibPlayerSpells-1.0')
 if not lib then return end
-lib:__RegisterSpells('SHAMAN', 11340, 2, {
+lib:__RegisterSpells('SHAMAN', 20501, 2, {
 	COOLDOWN = {
 		  17364, -- Stormstrike (Enhancement)
 		AURA = {
