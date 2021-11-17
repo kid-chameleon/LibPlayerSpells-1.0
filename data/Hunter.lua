b36ada4895c81b812ef4a7d0df5d7ed67df8502e
kid-chameleon <56284637+kid-chameleon@users.noreply.github.com>
2021-11-16 16:06:09 -0800

    update toc

--- data/Hunter.lua
+++ data/Hunter.lua
@@ -20,7 +20,7 @@ along with LibPlayerSpells-1.0. If not, see <http://www.gnu.org/licenses/>.
 
 local lib = LibStub('LibPlayerSpells-1.0')
 if not lib then return end
-lib:__RegisterSpells('HUNTER', 20501, 2, {
+lib:__RegisterSpells('HUNTER', 11401, 2, {
 	COOLDOWN = {
 		AURA = {
 			HARMFUL = {
